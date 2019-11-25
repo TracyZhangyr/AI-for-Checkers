3d21e5ee0d48d0910e0b4236810b4eec091074e9
Yuerong Zhang <38518111+TracyZhangyr@users.noreply.github.com>
2019-11-24 20:47:00 -0800

    Update run_test_ai

--- Checkers_Student/Tools/run_test_ai.py
+++ Checkers_Student/Tools/run_test_ai.py
@@ -8,15 +8,36 @@ command3 = "python3 /home/yuerongz/Checkers_Student/Tools/AI_Runner.py 7 7 2 l /
 command4 = "python3 /home/yuerongz/Checkers_Student/Tools/AI_Runner.py 7 7 2 l /home/yuerongz/Checkers_Student/src/checkers-python/main.py /home/yuerongz/Checkers_Student/Tools/Sample_AIs/Poor_AI/main.py"
 test_time = 50
 
-def run(i):
+def run1(i):
     start = time.time()
     os.system(command1)
+    print(i,time.time()-start)
+
+def run2(i):
+    start = time.time()
+    os.system(command2)
+    print(i,time.time()-start)
+
+def run3(i):
+    start = time.time()
+    os.system(command3)
+    print(i,time.time()-start)
+
+def run4(i):
+    start = time.time()
+    os.system(command4)
     print(i,time.time()-start)
 
 if __name__ == '__main__':
     p = Pool(test_time)
     for i in range(1, test_time+1):
-        p.apply_async(run, args=(i,))
+        # Our AI as player 2
+        #p.apply_async(run1, args=(i,))
+        #p.apply_async(run3, args=(i,))
+
+        #Our AI as player 1
+        p.apply_async(run2, args=(i,))
+        p.apply_async(run4, args=(i,))
     print("Start running")
     p.close()
     p.join()
