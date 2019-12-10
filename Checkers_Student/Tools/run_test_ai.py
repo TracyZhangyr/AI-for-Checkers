2651e3bc71a22fd8b133317f9880eba6af665bde
Yuerong Zhang <38518111+TracyZhangyr@users.noreply.github.com>
2019-12-09 22:07:04 -0800

    just test update

--- Checkers_Student/Tools/run_test_ai.py
+++ Checkers_Student/Tools/run_test_ai.py
@@ -14,7 +14,7 @@ command2 = "python3 /home/yuerongz/Checkers_Student/Tools/AI_Runner.py 9 8 2 l /
 command3 = "python3 /home/yuerongz/Checkers_Student/Tools/AI_Runner.py 7 7 2 l /home/yuerongz/Checkers_Student/Tools/Sample_AIs/Average_AI/main.py /home/yuerongz/Checkers_Student/src/checkers-python/main.py"
 command4 = "python3 /home/yuerongz/Checkers_Student/Tools/AI_Runner.py 7 7 2 l /home/yuerongz/Checkers_Student/src/checkers-python/main.py /home/yuerongz/Checkers_Student/Tools/Sample_AIs/Average_AI/main.py"
 
-test_time = 50
+test_time = 40
 
 def run1(i):
     start = time.time()
@@ -40,11 +40,11 @@ if __name__ == '__main__':
     p = Pool(test_time)
     for i in range(1, test_time+1):
         # Our AI as player 2
-        p.apply_async(run1, args=(i,))
+        #p.apply_async(run1, args=(i,))
         #p.apply_async(run3, args=(i,))
 
         #Our AI as player 1
-        #p.apply_async(run2, args=(i,))
+        p.apply_async(run2, args=(i,))
         #p.apply_async(run4, args=(i,))
     print("Start running")
     p.close()
